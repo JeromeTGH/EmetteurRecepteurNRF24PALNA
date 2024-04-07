6bfbe25e271002292759fc89c1b56b75ce07b42c
JeromeTGH <jerome.tomski@gmx.fr>
2024-04-07 17:03:31 +0200

    Création de la structure de base des programmes arduino

---
+++ prgArduinoEmetteur/prgArduinoEmetteur.ino
@@ -0,0 +1,41 @@
+/*
+   ______               _                  _///_ _           _                   _
+  /   _  \             (_)                |  ___| |         | |                 (_)
+  |  [_|  |__  ___  ___ _  ___  _ __      | |__ | | ___  ___| |_ _ __ ___  _ __  _  ___  _   _  ___
+  |   ___/ _ \| __|| __| |/ _ \| '_ \_____|  __|| |/ _ \/  _|  _| '__/   \| '_ \| |/   \| | | |/ _ \
+  |  |  | ( ) |__ ||__ | | ( ) | | | |____| |__ | |  __/| (_| |_| | | (_) | | | | | (_) | |_| |  __/
+  \__|   \__,_|___||___|_|\___/|_| [_|    \____/|_|\___|\____\__\_|  \___/|_| |_|_|\__  |\__,_|\___|
+                                                                                      | |
+                                                                                      \_|
+  Fichier :       prgArduinoEmetteur.ino  
+  Description :   Programme permettant de réaliser un émetteur radio, à base de module nRF24 + PA + LNA
+
+  Licence :       BY-NC-ND 4.0 CC (https://creativecommons.org/licenses/by-nc-nd/4.0/deed.fr)
+  
+  Remarques :     - le microcontrôleur utilisé ici sera un ATmega328P (version DIP)
+                  - la programmation du µC se fera via l'IDE Arduino, en utilisant FTDI comme passerelle
+                  - un sélecteur rotatif à 10 positions permettra de choisir l'une des dix fréquences de transmission possibles
+                  - l'émetteur dispose de 4 boutons poussoirs, qui piloteront les 4 relais disposés au niveau du récepteur correspondant
+
+  Dépôt GitHub :  https://github.com/JeromeTGH/EmetteurRecepteurNRF24PALNA (fichiers sources du projet, émetteur + récepteur)
+
+  Auteur :        Jérôme TOMSKI (https://passionelectronique.fr/)
+  Créé le :       07.04.2024
+  
+*/
+
+
+// ========================
+// Initialisation programme
+// ========================
+void setup() {
+
+}
+
+
+// =================
+// Boucle principale
+// =================
+void loop() {
+  
+}
